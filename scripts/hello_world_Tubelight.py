44d9438ac2e1dea5b6d805a9127675b37019c6e0
foreverGithub <33194390+foreverGithub@users.noreply.github.com>
2017-10-29 14:17:26 +0530

    Update hello_world_Tubelight.py

--- scripts/hello_world_Tubelight.py
+++ scripts/hello_world_Tubelight.py
@@ -1,6 +1,6 @@
-/ LANGUAGE: Python
-// ENV: Python
+// LANGUAGE: Python
+// ENV: Python 3
 // AUTHOR: Tubelight
 // GITHUB: https://github.com/tubelight36
 
-print('Hello World!');
+print('Hello World!')
